0843955f4b6ead12652b87f79c846ed4f0881862
Дмитрий Загребин <essergame@gmail.com>
2020-06-04 01:59:57 +0300

    Добавьте файлы проекта.

---
+++ Technology/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication444.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
